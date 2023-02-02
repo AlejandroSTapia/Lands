63c730bb6061657604a44bd1c716c182d31165f6
AlejandroSanchezTapia <neil.tapia11@gmail.com>
2023-02-02 17:05:58 -0600

    LoginPage

--- Lands/Lands/Infraestructure/InstanceLocator.cs
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -6,20 +6,16 @@ namespace Lands.Infraestructure
     using Lands.ViewModels;
     internal class InstanceLocator
     {
-        //Primero crearemos una propiedad Mian de la tipo mainvoewmodel dentro de la clase InstanceLocator
-        //SI escribimos prop y doble tab nos genra el bloque de cod pero lo sustituimos asi:
+        //Propiedad Main
         public MainViewModel Main
         {
             //public int MyProperty {
             get; set; 
         }
-        //luego creamos el constructor a la clase con ctor doble tab
         public InstanceLocator()
         {
-            //La propiedad Main es igual a MainViewModel
-            //Esto es un patron de diseño que llama al patron Locator o carretera despejada
             this.Main = new MainViewModel();
-            //Esto nos sirve para que podamos ligar la pagina Login al MainViewModel
+            //Para ligar la pagina Login al MainViewModel
         }
 
     }
